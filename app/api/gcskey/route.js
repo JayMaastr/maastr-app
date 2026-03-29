da9e7d23d67d8c31c5ecfa92b80c54bd92ec9c94
JayMaastr <jay@maastr.io>
2026-03-28 23:00:53 -0400

    temp: add CORS to key relay

--- app/api/gcskey/route.js
+++ app/api/gcskey/route.js
@@ -2,5 +2,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   const s = request.nextUrl.searchParams.get('s');
   if(s !== 'maastr-gcs-2026') return NextResponse.json({error:'no'},{status:403});
-  return NextResponse.json({k: process.env.GCS_SERVICE_ACCOUNT_KEY});
+  return NextResponse.json({k: process.env.GCS_SERVICE_ACCOUNT_KEY}, {
+    headers: {'Access-Control-Allow-Origin': '*'}
+  });
 }
